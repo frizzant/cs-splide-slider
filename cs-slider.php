1879f71836837ee747b6d54f39055f4756da451f
Erin <erin@webkinder.ch>
2022-03-31 17:42:28 +0200

    Initial after build

---
+++ cs-slider.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name:       CS Slider Block
+ * Plugin URI:        www.calmarsolutions.ch
+ * Description:       Displays Content in a Slider
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            Erin McGowan | Frizzant
+ * License:           GPLv3
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.en.html
+ * Text Domain:       cs-slider
+ *
+ * @package           cs-slider
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function cs_slider_cs_slider_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'cs_slider_cs_slider_block_init' );
