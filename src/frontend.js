10d85b90adacd27a4992ec6226bc313ec750522d
Erin <erindazil@gmail.com>
2022-06-10 14:25:39 +0200

    Refactor destruction of slider style if viewport is small

--- src/frontend.js
+++ src/frontend.js
@@ -4,14 +4,25 @@
 import Splide from '@splidejs/splide';
 import options from './options.json';
 
+const viewportWidth = document.documentElement.clientWidth;
+const destroyBlockStyle = (viewportWidth < 980);
+
 document.addEventListener( 'DOMContentLoaded', function() {
-	const splideSliders = document.querySelectorAll('.splide');
+	const splideSliders = document.querySelectorAll('.wp-block-cs-splide-slider');
 
 	/**
 	 * Initialize every Splide slider
 	 */
-	[...splideSliders].map(item => item).forEach(splideSlider => {
-		let splide = new Splide( splideSlider, options );
+	[...splideSliders].map(item => item).forEach(block => {
+		const sliderElement = block.querySelector('.splide');
+		let splide = new Splide( sliderElement, options );
+
 		splide.mount();
+
+		if ( destroyBlockStyle && block.classList.contains('is-style-mini-gallery') ) {
+			splide.destroy();
+		}
 	});
 } );
+
+//980
